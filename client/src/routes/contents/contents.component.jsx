84286e980048b001c7d23a5d95ba39cbb1af57c1
Didi Wu <devdidi5@gmail.com>
2022-08-19 13:00:29 -0700

    create server and prepare for contents component

--- client/src/routes/contents/contents.component.jsx
+++ client/src/routes/contents/contents.component.jsx
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import './contents.styles.scss';
 
 export default function Contents() {
+  const [] = useState([]);
+  // useEffect => load contents array
+  // useParams => if book title change => useEffect
+
   return <div>contents</div>;
 }
---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const fs = require('fs');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+// Init Middleware
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+  );
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
+
+app.get('/', (req, res) => res.send('API running'));
+
+// Define Routes
+app.use('/api/AnneOfGreenGables', require('./routes/api/AnneOfGreenGables'));
+
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
