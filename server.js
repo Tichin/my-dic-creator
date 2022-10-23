0068038aade0d0d21f88f504f17bfa3311f35c42
Didi <devdidi5@gmail.com>
2022-10-23 10:55:05 -0700

    Change ports to run temperarily

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const app = express();
 // Init Middleware
@@ -9,24 +9,24 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header("Access-Control-Allow-Origin", "*");
   res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
   );
   res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
   );
   next();
 });
 
-app.get('/', (req, res) => res.send('API running'));
+app.get("/", (req, res) => res.send("API running"));
 
 // Define Routes
-app.use('/api/AnneOfGreenGables', require('./routes/api/AnneOfGreenGables'));
-app.use('/api/Sapiens', require('./routes/api/Sapiens'));
+app.use("/api/AnneOfGreenGables", require("./routes/api/AnneOfGreenGables"));
+app.use("/api/Sapiens", require("./routes/api/Sapiens"));
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5002;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
