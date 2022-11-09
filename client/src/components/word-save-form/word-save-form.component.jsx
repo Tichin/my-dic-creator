ed14338d270f94b415c48ef67f036c61847de119
Didi <devdidi5@gmail.com>
2022-11-08 20:06:28 -0800

    modified styles

--- client/src/components/word-save-form/word-save-form.component.jsx
+++ client/src/components/word-save-form/word-save-form.component.jsx
@@ -1,20 +1,20 @@
-import React, { useState, useContext, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { CartContext } from '../../contexts/cart.context';
-import { WordContext } from '../../contexts/word.context';
-import './word-save-form.styles.scss';
+import React, { useState, useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { CartContext } from "../../contexts/cart.context";
+import { WordContext } from "../../contexts/word.context";
+import "./word-save-form1.styles.scss";
 
 const defaultFormFields = {
-  verb: '',
-  singular: '',
-  phrasal: '',
-  text: '',
-  partOfSpeech: '',
-  pastTense: '',
-  pastParticiple: '',
-  plural: '',
-  definition: '',
-  example: '', // from dictionanry
+  verb: "",
+  singular: "",
+  phrasal: "",
+  text: "",
+  partOfSpeech: "",
+  pastTense: "",
+  pastParticiple: "",
+  plural: "",
+  definition: "",
+  example: "", // from dictionanry
 };
 
 export default function WordSaveForm() {
@@ -52,109 +52,109 @@ export default function WordSaveForm() {
     event.preventDefault();
     updateItemToCart({ ...formFields });
     resetFormFields();
-    alert('save to basket successfully!');
+    alert("save to basket successfully!");
     navigate(-1);
   };
 
   return (
-    <div className='container'>
+    <div className="container">
       <form onSubmit={onFormSubmit}>
-        <ul className='flex-outer'>
+        <ul className="flex-outer">
           <li>
             <label>Text</label>
             <input
-              type='text'
+              type="text"
               onChange={handleChange}
               value={text}
-              name='text'
+              name="text"
             ></input>
           </li>
           <li>
             <label>Verb</label>
             <input
-              type='text'
+              type="text"
               onChange={handleChange}
               value={verb}
-              name='verb'
+              name="verb"
             ></input>
           </li>
           <li>
             <label>Singular</label>
             <input
-              type='text'
+              type="text"
               onChange={handleChange}
               value={singular}
-              name='singular'
+              name="singular"
             ></input>
           </li>
           <li>
             <label>Phrasal</label>
             <input
-              type='text'
+              type="text"
               onChange={handleChange}
               value={phrasal}
-              name='phrasal'
+              name="phrasal"
             ></input>
           </li>
           <li>
             <label>Part Of Speech</label>
             <input
-              type='text'
+              type="text"
               onChange={handleChange}
               value={partOfSpeech}
-              name='partOfSpeech'
+              name="partOfSpeech"
             ></input>
           </li>
           <li>
             <label>Past Tense</label>
             <input
-              type='text'
+              type="text"
               onChange={handleChange}
               value={pastTense}
-              name='pastTense'
+              name="pastTense"
             ></input>
           </li>
           <li>
             <label>Past Participle</label>
             <input
-              type='text'
+              type="text"
               onChange={handleChange}
               value={pastParticiple}
-              name='pastParticiple'
+              name="pastParticiple"
             />
           </li>
           <li>
             <label>Plural</label>
             <input
-              type='text'
+              type="text"
               onChange={handleChange}
-              name='plural'
+              name="plural"
               value={plural}
             />
           </li>
           <li>
             <label>Definition</label>
-            <input
-              type='text'
+            <textarea
+              type="text"
               onChange={handleChange}
-              name='definition'
+              name="definition"
               value={definition}
             />
           </li>
           <li>
             <label>Example</label>
             <textarea
-              rows='4'
-              cols='200'
+              rows="4"
+              cols="200"
               onChange={handleChange}
-              name='example'
+              name="example"
               value={example}
             >
               exmaples...
             </textarea>
           </li>
           <li>
-            <button type='submit'>Save to Basket</button>
+            <button type="submit">Save to Basket</button>
           </li>
         </ul>
       </form>
